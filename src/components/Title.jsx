cf6bbd581d2eb8b76be22bd868bc32bc8812a247
Soham Datta <sohamdatta34@gmail.com>
2023-09-04 17:10:03 +0530

    chore: title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,7 +3,7 @@ export default function Title() {
         <div className="container mx-auto">
             <div className="m-5 p-4 shadow-lg bg-zinc-400">
                 <h1 className="text-5xl font-bold text-center">critic.ai</h1>
-                <p className="text-xl leading-relaxed text-center">twin bitches hoppin off a jetskii</p>
+                <p className="text-xl leading-relaxed text-center">jetskii</p>
             </div>
         </div>
     );
